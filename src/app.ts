53e68d1da5eedaa7cd90aefa414ba4f418205ed2
Jeff Guo <sguo35@gmail.com>
2018-06-16 13:06:08 -0700

    Finish API

--- src/app.ts
+++ src/app.ts
@@ -32,16 +32,6 @@ import * as passportConfig from "./config/passport";
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-const mongoUrl = MONGODB_URI;
-(<any>mongoose).Promise = bluebird;
-mongoose.connect(mongoUrl, {useMongoClient: true}).then(
-  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch((err: any) => {
-  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-  // process.exit();
-});
-
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
@@ -49,39 +39,17 @@ app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: SESSION_SECRET,
-  store: new MongoStore({
-    url: mongoUrl,
-    autoReconnect: true
-  })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+
+
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use((req: any, res: any, next: any) => {
   res.locals.user = req.user;
   next();
 });
-app.use((req: any, res: any, next: any) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-    req.path !== "/login" &&
-    req.path !== "/signup" &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
-    req.path == "/account") {
-    req.session.returnTo = req.path;
-  }
-  next();
-});
+
+
+app.post('/api/getVacation', apiController.getVacation);
 
 app.use(
   express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -5,6 +5,7 @@ import request from "request";
 import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
+import "isomorphic-fetch";
 
 /**
  * GET /api
@@ -17,25 +18,213 @@ export let getApi = (req: Request, res: Response) => {
 };
 
 interface GetVacation extends Request {
-  tastes: Array<string>,
-  location: string,
-  interests: Array<string>,
-  startTime: string,
-  endTime: string,
-  budget: number
+  body: {
+    tastes: string;
+    location: string;
+    interests: string;
+    startTime: string;
+    endTime: string;
+    budget: number;
+    startLocation: string;
+  }
 };
+const yelp = require('yelp-fusion');
 
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey = process.env.YELP_API;
+
+
+
+const getData = async (loc: any, pref: any) => {
+  const searchRequest = {
+    location: loc,
+    term: pref,
+    limit: 5
+  };
+  //console.log(searchRequest);
+  const client = yelp.client(apiKey);
+
+  const response = await client.search(searchRequest);
+  return response.jsonBody.businesses;
+}
 export let getVacation = async (req: GetVacation, res: Response) => {
   // query Yelp for each of the tastes and interests, according to the budget guidelines
+  // get breakfast restaurants
+  let breakfastArr = Array<any>(0);
+  let breakfasts = req.body.tastes.split(" ");
+  for (let taste of breakfasts) {
+    breakfastArr = breakfastArr.concat(await getData(req.body.location, taste + " breakfast"));
+  }
 
-  // call our optimization algorithm to take the Yelp areas of attraction, time frame to create itinerary
+  // get restaurants
+  let tastesArr = Array<any>(0);
+  let tastes = req.body.tastes.split(" ");
+  for (let taste of tastes) {
+    tastesArr = tastesArr.concat(await getData(req.body.location, taste));
+  }
 
-  // return the 2D array result
-  const result = {};
-  res.json(result);
-  res.statusCode(200);
-  res.end();
+  let interestsArr = Array<any>(0);
+  let interests = req.body.interests.split(" ");
+  for (let interest of interests) {
+    interestsArr = interestsArr.concat(await getData(req.body.location, interest));
+  }
+
+  // query for hotels
+  let hotels: any = await getData(req.body.location, "hotels");
+
+  let googleMapsClient = require('@google/maps').createClient({
+    key: process.env.GOOGLE_API_KEY
+  });
+
+  let destinationAirport: any = "";
+  googleMapsClient.places({
+    query: `international airport near ${req.body.location}`
+  }, function (err: any, response: any) {
+    if (!err) {
+      destinationAirport = response.json.results[0];
+    } else {
+      console.log(err);
+    }
+  });
+
+  let startAirport: any = "";
+  googleMapsClient.places({
+    query: `international airport near ${req.body.startLocation}`
+  }, async function (err: any, response: any) {
+    if (!err) {
+      startAirport = response.json.results[0];
+      //console.log(startAirport);
+      let airTransport = {
+        startAirport: startAirport,
+        endAirport: destinationAirport,
+        price: Math.round((Math.floor(Math.random() * 23) + 20)
+          * Math.sqrt((
+            Math.pow(startAirport.geometry.location.lng - destinationAirport.geometry.location.lng, 2) +
+            Math.pow(startAirport.geometry.location.lat - destinationAirport.geometry.location.lat, 2))))
+      }
+
+      // return the array result
+      const result = optimize(interestsArr, breakfastArr, tastesArr, hotels, airTransport, new Date(req.body.startTime), new Date(req.body.endTime), req.body.budget);
+
+      const outResult = {
+        itinerary: result,
+        transportation: airTransport
+      }
+
+      res.json(outResult);
+      res.status(200);
+      res.end();
+    }
+  });
+
+};
+
+interface DayItinerary {
+  breakfast: any;
+  lunch: any;
+  dinner: any;
+  hotel: any;
+  attractions: any;
+  date: string;
+  moneySpent: number;
+  [key: string]: any;
 }
+
+/**
+ * Creates an optimal itinerary for a vacation given the fetched attractions, restaurants, and hotels.
+ * @param attractions 
+ * @param restaurants 
+ * @param hotels 
+ */
+const optimize = (attractions: Array<any>, breakfastRestaurants: Array<any>, restaurants: Array<any>,
+  hotels: any, transportationAir: Object,
+  startTime: Date, endTime: Date, budget: number): any => {
+  // The cost is defined by either the dollar sign bracket if it's a restaurant
+  // or the actual dollar cost as defined by Expedia/Uber if it's a hotel/transportation/plane ride.
+
+  // compute the number of days this trip will take
+  let difference = endTime.getTime() - startTime.getTime();
+  difference /= (1000 * 60 * 60 * 24);
+  difference = Math.round(difference);
+
+  let stopSearching: boolean = false;
+
+  let outObj = new Array<DayItinerary>(0);
+  let moneySpent: number = 0;
+
+  const priceLookup = {
+    'restaurant': 10,
+    'attraction': 30
+  }
+
+  // while we still have money, keep spending to fill out the day and if we finish looping
+  // then start upgrading stuff
+  let hotel: any = "";
+  // find a hotel
+  hotel = hotels[0];
+  hotel.price = hotel.price.length * (Math.random() * 200 + 150);
+  for (let day = 0; day < difference && moneySpent < budget; day++) {
+    moneySpent += hotel.price;
+
+    let dayIt: DayItinerary = {
+      breakfast: {},
+      lunch: {},
+      dinner: {},
+      hotel: hotel,
+      attractions: [],
+      date: (new Date(startTime.getTime() + (1000 * 60 * 60 * 24 * day))).toISOString(),
+      moneySpent: 0
+    }
+
+    for (let objKey in dayIt) {
+      // check if it'll go over our budget
+      if (moneySpent > budget) {
+        break;
+      }
+
+      if (objKey == "attractions") {
+        // find 3 attractions
+        for (let i = 0; i < 3; i++) {
+          dayIt[objKey].push(attractions[Math.floor(Math.random() * attractions.length)])
+          //console.log(dayIt[objKey][i])
+          if (dayIt[objKey][i].price)
+            {dayIt[objKey][i].price = priceLookup['attraction'] * dayIt[objKey][i].price.length;}
+          else
+            {dayIt[objKey][i].price = priceLookup['attraction'] * Math.random() * 3 + 1;}
+          if (dayIt[objKey][i].price == null) {
+            console.log("TESTESTTESTTEST");
+          }
+          moneySpent += dayIt[objKey][i].price;
+        }
+      } else if (objKey == "breakfast") {
+        dayIt[objKey] = breakfastRestaurants[Math.floor(Math.random() * breakfastRestaurants.length)];
+        if (dayIt[objKey].price)
+          {dayIt[objKey].price = priceLookup['restaurant'] * dayIt[objKey].price.length;}
+        else
+          {dayIt[objKey].price = priceLookup['restaurant'] * Math.random() * 3 + 1;}
+
+        if (dayIt[objKey].price == null) {
+          console.log("TESTESTTESTTEST");
+        }
+        moneySpent += dayIt[objKey].price;
+      } 
+      else if (objKey != "hotel" && objKey != "date" && objKey != "breakfast") {
+
+        // otherwise just compute the price and add it
+        dayIt[objKey] = restaurants[Math.floor(Math.random() * restaurants.length)];
+        if (dayIt[objKey].price)
+          {dayIt[objKey].price = priceLookup['restaurant'] * dayIt[objKey].price.length;}
+        else
+          {dayIt[objKey].price = priceLookup['restaurant'] * Math.random() * 3 + 1;}
+        moneySpent += dayIt[objKey].price;
+      }
+    }
+    dayIt["moneySpent"] = moneySpent;
+    outObj.push(dayIt);
+  }
+  return outObj;
+};
 
 /**
  * GET /api/facebook
